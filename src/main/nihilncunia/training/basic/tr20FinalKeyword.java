b45250f4341f02fdd9d3d30e83afc6854c5e8ef1
NIHILncunia <nihil_ncunia@naver.com>
2021-05-03 11:39:08 +0900

    2021 0503 클래스에 대한 심도있는 이해

--- src/main/nihilncunia/training/basic/tr20FinalKeyword.java
+++ src/main/nihilncunia/training/basic/tr20FinalKeyword.java
@@ -19,6 +19,30 @@ public class tr20FinalKeyword {
     // 그래서 변하면 안되는 값에 final을 붙이면 좋을 것 같다.
     
     // final 키워드는 클래스나 메소드에도 그대로 적용된다.
+    
+    Test2 ft2 = new Test2(20);
+  
+    System.out.println("plus >> " + ft2.plus(2));
+    System.out.println("toString >> " + ft2.toString().getClass());
+  }
+}
+
+class Test2 {
+  public int Number;
+  
+  public Test2(int _Number) {
+    Number = _Number;
+  }
+  // 생성자 메소드는 클래스와 같은 이름의 메소드이다.
+  // 인스턴스를 만들 때에 괄호안에 들어갈 값을 설정할 수 있다.
+  // 또한 this라는 키워드는 인스턴스 자신을 가리키는 키워드이다.
+  
+  public int plus(int num) {
+    return Number + num;
+  }
+  
+  public String toString() {
+    return Number + "는 문자열이 되었다!";
   }
 }
 
@@ -33,21 +57,33 @@ final class FinalTest {
 // 이렇게 하면 오류가 나게 된다.
 
 class FinalTest2 {
-  static final int number(int num) {
+  public int Number;
+  
+  public FinalTest2(int _Number) {
+    this.Number = _Number;
+  }
+  
+  public final int number(int num) {
     return num;
   }
   
-  static final int number(int num1, int num2) {
+  public final int number(int num1, int num2) {
     return num1 + num2;
   }
   // 클래스의 안에다가 메소드를 하나 만들었다.
   // 이 메소드는 final이 붙어있다. final이 붙어있는 메소드는 상속을 받은 클래스에서 오버라이딩. 즉, 재정의가 불가능하다.
   // 오버로딩은 여전히 가능하다.
+  
+  public String message = String.valueOf(Number);
 }
 
 class FinalTest3 extends FinalTest2 {
+  public FinalTest3(int _Number) {
+    super(_Number);
+  }
+  
   public static void main(String[] args) {
-    System.out.println(number(10));
+    // System.out.println(number(10));
     // 이렇게 사용하는 것은 아무런 문제가 없다.
     // 하지만 아래에서처럼 재정의를 시도하려고 하면 오류가 나게 된다.
   }
